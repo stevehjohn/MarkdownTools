864f828dcd8fd6abbcd80b23c29e6f2c6f008b5e
Stevö John <stevehuwjohn@gmail.com>
2019-02-20 21:34:33 +0000

    WIP - Adding Tests

--- src/MarkdownTools.Parser.Tests/Extensions/StringExtensionsTests.cs
+++ src/MarkdownTools.Parser.Tests/Extensions/StringExtensionsTests.cs
@@ -1,19 +1,33 @@
-﻿using NUnit.Framework;
+﻿using MarkdownTools.Parser.Extensions;
+using NUnit.Framework;
 
 namespace MarkdownTools.Parser.Tests.Extensions
 {
     [TestFixture]
     public class StringExtensionsTests
     {
+        [TestCase("", 1, null)]
+        [TestCase("Steve", 1, "teve")]
+        [TestCase("Steve", 5, null)]
+        [TestCase("", 0, null)]
         public void Exercise_SafeSubstring_overload_1(string input, int startIndex, string expected)
         {
+            Assert.That(input.SafeSubstring(startIndex), Is.EqualTo(expected));
         }
 
-        public void Exercise_SafeSubstring_overload_2(string input, int startIndex, string expected)
+        [TestCase("", 1, 10, null)]
+        [TestCase("Steve", 1, 10, "teve")]
+        [TestCase("Steve", 5, 10, null)]
+        [TestCase("", 0, 10, null)]
+        public void Exercise_SafeSubstring_overload_2(string input, int startIndex, int length, string expected)
         {
+            Assert.That(input.SafeSubstring(startIndex, length), Is.EqualTo(expected));
         }
 
-        public void Exercise_SafeGetChar(string input, int index)
+        [TestCase("Steve", 0, 'S')]
+        [TestCase("Steve", 4, 'e')]
+        [TestCase("Steve", 5, '\0')]
+        public void Exercise_SafeGetChar(string input, int index, char expected)
         {
         }
     }
