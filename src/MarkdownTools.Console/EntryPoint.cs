9f54d09b2bcb63f00418a6e1a015c110642dbf72
Stevo John <steve.john@just-eat.com>
2019-02-22 10:41:38 +0000

    Starting console app

--- src/MarkdownTools.Console/EntryPoint.cs
+++ src/MarkdownTools.Console/EntryPoint.cs
@@ -1,9 +1,20 @@
-﻿namespace MarkdownTools.Console
+﻿using CommandLine;
+using MarkdownTools.Console.Infrastructure;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MarkdownTools.Console
 {
+    [ExcludeFromCodeCoverage]
     public static class EntryPoint
     {
         public static void Main(string[] args)
         {
+            Parser.Default
+                  .ParseArguments<Options>(args)
+                  .WithParsed(o =>
+                  {
+
+                  });
         }
     }
 }
---
+++ src/MarkdownTools.Console/Infrastructure/Options.cs
@@ -0,0 +1,10 @@
+﻿namespace MarkdownTools.Console.Infrastructure
+{
+    public class Options
+    {
+        public string InputFile { get; set; }
+        public string OutputFile { get; set; }
+        public string Theme { get; set; }
+        public string CustomTheme { get; set; }
+    }
+}
