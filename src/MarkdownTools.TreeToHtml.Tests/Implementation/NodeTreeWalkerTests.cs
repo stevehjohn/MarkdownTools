da5a3bde2f9339ad1fd5889a5041e6fe0dca167f
Stevo John <steve.john@just-eat.com>
2019-03-08 20:20:22 +0000

    Removing tests that are covered elsewhere

--- src/MarkdownTools.TreeToHtml.Tests/Implementation/NodeTreeWalkerTests.cs
+++ src/MarkdownTools.TreeToHtml.Tests/Implementation/NodeTreeWalkerTests.cs
@@ -16,33 +16,6 @@ namespace MarkdownTools.TreeToHtml.Tests.Implementation
             _walker = new NodeTreeWalker();
         }
 
-        [Test]
-        public void Processes_blockquote_node()
-        {
-            _walker.LoadTree(new Node
-                             {
-                                 Type = NodeType.Root,
-                                 Children =
-                                 {
-                                     new Node
-                                     {
-                                         Type = NodeType.Blockquote,
-                                         Content = "This is a blockquote."
-                                     }
-                                 }
-                             });
-
-            var result = _walker.ToHtml(Theme.Raw);
-
-            Assert.That(result, Is.EqualTo("    <blockquote>\n        This is a blockquote.\n    </blockquote>\n".Replace("\n", Environment.NewLine)));
-        }
-
-        [Test]
-        public void Processes_heading_node()
-        {
-            _walker.LoadTree(new Node());
-        }
-
         [TestCase(Theme.Dark)]
         [TestCase(Theme.Light)]
         [TestCase(Theme.Custom, "TestFiles\\CustomTheme.html")]
