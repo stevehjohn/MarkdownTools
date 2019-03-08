5f5cc7d24a64d7d283193f1cc0650f34bb339ebe
Stevo John <steve.john@just-eat.com>
2019-03-08 19:28:18 +0000

    Added LinkEvaluator

---
+++ src/MarkdownTools.Parser.Tests/Implementation/Evaluators/LinkEvaluatorTests.cs
@@ -0,0 +1,39 @@
+﻿using MarkdownTools.Parser.Implementation.Evaluators;
+using MarkdownTools.Parser.Implementation.Evaluators.Base;
+using NUnit.Framework;
+
+namespace MarkdownTools.Parser.Tests.Implementation.Evaluators
+{
+    [TestFixture]
+    public class LinkEvaluatorTests
+    {
+        private IEvaluator _evaluator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _evaluator = new LinkEvaluator();
+        }
+
+        [TestCase("[A Link to Google](https://www.google.com)", "A Link to Google", "https://www.google.com")]
+        [TestCase("[Not a link", null, null)]
+        [TestCase("[", null, null)]
+        [TestCase("[Not a link either]", null, null)]
+        [TestCase("[Nor is this](", null, null)]
+        [TestCase("[Or this](http://www", null, null)]
+        public void Identifies_links(string input, string text, string link)
+        {
+            var result = _evaluator.Evaluate(input);
+
+            if (text != null)
+            {
+                Assert.That(result.Node.Content, Is.EqualTo(text));
+                Assert.That(result.Node.MetaData["Link"], Is.EqualTo(link));
+            }
+            else
+            {
+                Assert.Null(result);
+            }
+        }
+    }
+}
---
+++ src/MarkdownTools.Parser/Implementation/Evaluators/LinkEvaluator.cs
@@ -0,0 +1,56 @@
+﻿using MarkdownTools.Models;
+using MarkdownTools.Parser.Extensions;
+using MarkdownTools.Parser.Implementation.Evaluators.Base;
+using System;
+using System.Collections.Generic;
+
+namespace MarkdownTools.Parser.Implementation.Evaluators
+{
+    public class LinkEvaluator : IEvaluator
+    {
+        public NodeType IsEvaluatorFor => NodeType.Link;
+
+        public EvaluatorResult Evaluate(string source)
+        {
+            if (source.StartsWith("["))
+            {
+                var line = source.GetLine();
+
+                if (line.Length == 1)
+                {
+                    return null;
+                }
+
+                var end = line.IndexOf("](", StringComparison.Ordinal);
+
+                if (end == -1)
+                {
+                    return null;
+                }
+
+                var content = line.SafeSubstring(1, end - 1);
+
+                var linkEnd = line.IndexOf(')', end + 2);
+
+                if (linkEnd == -1)
+                {
+                    return null;
+                }
+
+                return new EvaluatorResult(
+                    new Node
+                    {
+                        Type = NodeType.Link,
+                        Content = content,
+                        MetaData = new Dictionary<string, string>
+                                   {
+                                       { "Link", line.Substring(end + 2, linkEnd - end - 2) }
+                                   }
+                    },
+                    source.SafeSubstring(linkEnd + 1));
+            }
+
+            return null;
+        }
+    }
+}
